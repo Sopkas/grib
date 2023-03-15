9ac2d6a6c73b27489271b09f93aec3781f22eac1
Sopkas <andreieshtokin2015@gmail.com>
2023-03-16 00:02:15 +1000

    Revert "AnimatioPlayers"
    This reverts commit 6d8ee662d8f44f72e2c12c27c8aa3bb8d0731049.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,49 +9,17 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveIntput;
     private Vector2 moveVelocity;
-    private Animator animator;
 
-    private bool facingRight = true;
-
-    void Start() 
-    {
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
-     void Update() 
-     {
+     void Update() {
         moveIntput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveIntput.normalized * speed;
-
-        if(moveIntput.x == 0) 
-        {
-            animator.SetBool("isRunning", false);
-        } else 
-        {
-            animator.SetBool("isRunning", true);
-        }
-
-        if (!facingRight && moveIntput.x > 0) 
-        {
-            Flip();
-        }
-        else if (facingRight && moveIntput.x < 0) 
-        {
-            Flip();
-        }
     }
 
-     void FixedUpdate() 
-     {
+     void FixedUpdate() {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
-    }
-
-    private void Flip() 
-    { 
-        facingRight = !facingRight;
-        Vector3 Scaler = transform.localScale;
-        Scaler.x *= -1;
-        transform.localScale = Scaler;
     }
 }
